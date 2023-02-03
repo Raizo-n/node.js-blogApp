5adbd6d9f9611d818c00812bbd948ab303a0fbb4
“Raizo-n” <sanpincha0826@gmail.com>
2023-02-02 23:24:48 -0800

    commit

---
+++ api/routes/posts.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  putPost,
+} from "../controllers/post.js";
+
+const router = express.Router();
+
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", putPost);
+
+export default router;
